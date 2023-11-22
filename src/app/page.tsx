dc5b6a4d6f0f304673b92bafd609aceb2f6aa6a7
Pranav Ruparelia <pranavruparelia9@gmail.com>
2023-11-22 00:13:31 -0500

    feat: intro to mui with example

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import styles from './page.module.css'
+import * as React from 'react';
+import Button from '@mui/material/Button';
 
 export default function Home() {
   return (
     <main className={styles.main}>
       {'React Notes App'}
+      <Button variant="contained">Hello world</Button>;
     </main>
   )
 }
